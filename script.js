9b319e028b257cf5850ecfbfbe3cce048a3f3554
Alimbabaeva <113401296+Alimbabaeva@users.noreply.github.com>
2023-05-23 23:27:18 +0600

    hard

--- script.js
+++ script.js
@@ -143,3 +143,36 @@ var mergeKLists = function(lists) {
     
     return r;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    if(root === null) {
+        return 0;
+    }
+    let sum = Number.NEGATIVE_INFINITY;
+    const rec = (node) => {
+        if(node === null) {
+            return 0;
+        }
+
+        const left = Math.max(rec(node.left) , 0);
+        const right = Math.max(rec(node.right) , 0);
+        sum = Math.max(left+right+node.val , sum);
+        return Math.max(left,right) + node.val;
+
+    };
+    rec(root);
+    return sum;
+};
