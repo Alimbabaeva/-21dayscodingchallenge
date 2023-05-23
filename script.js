f951e22731cd6c5d7634b4b74ceb588b94d05d17
Alimbabaeva <113401296+Alimbabaeva@users.noreply.github.com>
2023-05-23 23:29:55 +0600

    a

--- script.js
+++ script.js
@@ -192,3 +192,21 @@ const combinationSum2 = (candidates, target) => {
     helper([], target, 0);
     return res;
   };
+
+  //32. Longest Valid Parentheses
+  var longestValidParentheses = function(s) {
+    const n = s.length;
+    const S = [-1];
+    let x = 0;
+    for (let i = 0; i < n; ++i)
+        if (s[i] === '(') 
+            S.push(i); // open; mark start index
+        else {
+            S.pop(); // close; remove a start index
+            if (!S.length) 
+                S.push(i); // invalid; reset index to new start
+            else 
+                x = Math.max(x, i - S[S.length - 1]); // valid; save the length
+        }
+    return x;
+};
